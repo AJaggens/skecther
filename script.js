4d4bec64a6a392e397a0e36ad262d1932040b4a8
AJaggens <misteryugine@gmail.com>
2022-03-13 18:48:48 +0300

    grid generation and reset functionalit, picking dimensions

---
+++ script.js
@@ -0,0 +1,50 @@
+const gridContainer = document.getElementById('cell-container')
+let gridSide = 4
+let cellCount = gridSide ** 2
+let cellSide = 896 / gridSide
+
+generateGrid(cellCount)
+
+const gridControl = document.getElementById('grid-size')
+gridControl.addEventListener(`input`, function() {
+    clearGrid()
+    gridSide = gridControl.value * 1
+    cellCount = gridSide ** 2
+    cellSide = 896 / gridSide
+    generateGrid(cellCount)
+    console.log(gridSide)
+})
+
+//grid generator
+function generateGrid(count) {
+    const cells = []
+    fillCellArray(count, cells)
+    cells.forEach(e => {
+        gridContainer.appendChild(e)
+    });
+}
+
+//create an array of cells to fill container with assigned id class and height width
+function fillCellArray(count, array) {
+    for (let i = 0; i < count; i++ ) {
+        const cell = document.createElement('div')
+        cell.classList.add('grid-cell')
+        cell.setAttribute('id', `${i}`)
+        cell.style.width = `${cellSide}px`
+        cell.style.height = `${cellSide}px`
+        array.push(cell)
+    }
+}
+
+//clear prev cells
+function clearGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild)
+    }
+}
+
+
+
+
+
+
